38a68fdfe23d0b30210d872618e8647cf8baa2ab
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-03-17 19:42:09 +0100

    Switch Android launcher to Flutter and build APK via Flutter in CI

--- android/app/src/main/kotlin/com/maamahsmix/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/maamahsmix/app/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.maamahsmix.app
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : AppCompatActivity() {
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
-   }
-}
+class MainActivity : FlutterActivity()
