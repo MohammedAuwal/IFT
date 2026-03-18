fb1a9aa8a6172e29caafe759542330cb32e5dcc5
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-03-18 08:08:47 +0100

    Initialize Firebase in main before app startup

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:mix/features/admin/presentation/screens/admin_dashboard_screen.dart';
@@ -16,60 +15,26 @@ class MixBootstrap extends StatelessWidget {
   Widget build(BuildContext context) {
     final auth = FirebaseAuthService();
 
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, firebaseSnap) {
-        if (firebaseSnap.connectionState != ConnectionState.done) {
-          return const MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            ),
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges,
+      builder: (context, authSnap) {
+        if (authSnap.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
           );
         }
 
-        if (firebaseSnap.hasError) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(
-              body: Center(
-                child: Padding(
-                  padding: EdgeInsets.all(24),
-                  child: Text(
-                    'Firebase initialization failed.',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          );
+        final user = authSnap.data;
+
+        if (user == null) {
+          return const LoginScreen();
         }
 
-        return StreamBuilder<User?>(
-          stream: auth.authStateChanges,
-          builder: (context, authSnap) {
-            if (authSnap.connectionState == ConnectionState.waiting) {
-              return const MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: Scaffold(
-                  body: Center(child: CircularProgressIndicator()),
-                ),
-              );
-            }
+        if (user.uid == kAdminUid) {
+          return AdminDashboardScreen();
+        }
 
-            final user = authSnap.data;
-
-            if (user == null) {
-              return const LoginScreen();
-            }
-
-            if (user.uid == kAdminUid) {
-              return AdminDashboardScreen();
-            }
-
-            return ProductListScreen();
-          },
-        );
+        return ProductListScreen();
       },
     );
   }
