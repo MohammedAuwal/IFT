59556c8bbb2fb7725d2b6ac63a8e39d95bc3ad1f
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-03-18 08:09:27 +0100

    Configure Cloudinary and Firestore rules for product images

--- lib/core/constants/cloudinary_config.dart
+++ lib/core/constants/cloudinary_config.dart
@@ -1,6 +1,6 @@
 class CloudinaryConfig {
-  static const String cloudName = 'PUT_CLOUD_NAME_HERE';
-  static const String uploadPreset = 'PUT_UNSIGNED_UPLOAD_PRESET_HERE';
+  static const String cloudName = 'dhzzhyhqy';
+  static const String uploadPreset = 'MaamahsMix';
 
   static String get uploadUrl =>
       'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
