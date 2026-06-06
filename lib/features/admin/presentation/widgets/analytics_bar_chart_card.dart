abf9aefa0f4bdc8af8d03f630d5123f0903a209a
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 13:03:45 +0100

    Change import paths for app_surface_card and theme

--- lib/features/admin/presentation/widgets/analytics_bar_chart_card.dart
+++ lib/features/admin/presentation/widgets/analytics_bar_chart_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/shared/widgets/app_surface_card.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/shared/widgets/app_surface_card.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class AnalyticsBarChartCard extends StatelessWidget {
   final String title;
