0b8df9eb4887892f891a5e51b166458bc7f7a3d3
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 13:24:03 +0100

    Update location_search_field.dart

--- lib/features/rider/presentation/widgets/location_search_field.dart
+++ lib/features/rider/presentation/widgets/location_search_field.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/models/place_suggestion_model.dart';
-import 'package:mix/services/geocoding_service.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/models/place_suggestion_model.dart';
+import 'package:ift/services/geocoding_service.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class LocationSearchField extends StatefulWidget {
   final TextEditingController controller;
