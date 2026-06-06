e0b9affa041001d29653360a742acb1324f8f877
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 13:28:51 +0100

    Update splash_screen.dart

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/config/routes/route_names.dart';
-import 'package:mix/core/routing/app_router.dart';
-import 'package:mix/core/theme/app_theme.dart';
-import 'package:mix/services/firebase_service.dart';
+import 'package:ift/config/routes/route_names.dart';
+import 'package:ift/core/routing/app_router.dart';
+import 'package:ift/core/theme/app_theme.dart';
+import 'package:ift/services/firebase_service.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
