4b19d4fca1f137152d17f409b68291e33c37f825
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 08:18:32 +0100

    Update add_product_screen.dart

--- lib/features/admin/presentation/screens/add_product_screen.dart
+++ lib/features/admin/presentation/screens/add_product_screen.dart
@@ -4,16 +4,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:mix/features/products/data/product_repository.dart';
-import 'package:mix/models/product_model.dart';
-import 'package:mix/services/cloudinary_service.dart';
-import 'package:mix/services/firebase_service.dart';
-import 'package:mix/services/image_pick_service.dart';
-import 'package:mix/shared/widgets/app_form_field.dart';
-import 'package:mix/shared/widgets/app_page_scaffold.dart';
-import 'package:mix/shared/widgets/app_section_title.dart';
-import 'package:mix/shared/widgets/app_surface_card.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/features/products/data/product_repository.dart';
+import 'package:ift/models/product_model.dart';
+import 'package:ift/services/cloudinary_service.dart';
+import 'package:ift/services/firebase_service.dart';
+import 'package:ift/services/image_pick_service.dart';
+import 'package:ift/shared/widgets/app_form_field.dart';
+import 'package:ift/shared/widgets/app_page_scaffold.dart';
+import 'package:ift/shared/widgets/app_section_title.dart';
+import 'package:ift/shared/widgets/app_surface_card.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class AddProductScreen extends StatefulWidget {
   const AddProductScreen({super.key});
