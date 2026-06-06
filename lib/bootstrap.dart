25b3acb375d33f8cee8f27e974b32b6f7c8ced92
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 07:48:21 +0100

    Update bootstrap.dart

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mix/features/splash/presentation/screens/splash_screen.dart';
+import 'package:ift/features/splash/presentation/screens/splash_screen.dart';
 
-class MixBootstrap extends StatelessWidget {
+class IftBootstrap extends StatelessWidget {
   const MixBootstrap({super.key});
 
   @override
