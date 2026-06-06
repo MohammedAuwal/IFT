d6ec6b5db4ef8871d31ae40b99cc7d1afe165a38
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 13:01:56 +0100

    Update payment_settings_screen.dart

--- lib/features/admin/presentation/screens/payment_settings_screen.dart
+++ lib/features/admin/presentation/screens/payment_settings_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/models/payment_config_model.dart';
-import 'package:mix/services/payment_service.dart';
-import 'package:mix/shared/widgets/app_page_scaffold.dart';
-import 'package:mix/shared/widgets/app_section_title.dart';
-import 'package:mix/shared/widgets/app_surface_card.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/models/payment_config_model.dart';
+import 'package:ift/services/payment_service.dart';
+import 'package:ift/shared/widgets/app_page_scaffold.dart';
+import 'package:ift/shared/widgets/app_section_title.dart';
+import 'package:ift/shared/widgets/app_surface_card.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class PaymentSettingsScreen extends StatefulWidget {
   const PaymentSettingsScreen({super.key});
