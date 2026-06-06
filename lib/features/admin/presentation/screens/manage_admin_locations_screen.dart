a6f697b1881c7dc742fc30d08f34fe0195bf9a2d
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 12:59:36 +0100

    Update manage_admin_locations_screen.dart

--- lib/features/admin/presentation/screens/manage_admin_locations_screen.dart
+++ lib/features/admin/presentation/screens/manage_admin_locations_screen.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/features/shared/presentation/widgets/premium_location_picker_bottom_sheet.dart';
-import 'package:mix/models/place_suggestion_model.dart';
-import 'package:mix/services/firebase_service.dart';
-import 'package:mix/shared/widgets/app_bottom_sheets.dart';
-import 'package:mix/shared/widgets/app_form_field.dart';
-import 'package:mix/shared/widgets/app_page_scaffold.dart';
-import 'package:mix/shared/widgets/app_section_title.dart';
-import 'package:mix/shared/widgets/app_surface_card.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/features/shared/presentation/widgets/premium_location_picker_bottom_sheet.dart';
+import 'package:ift/models/place_suggestion_model.dart';
+import 'package:ift/services/firebase_service.dart';
+import 'package:ift/shared/widgets/app_bottom_sheets.dart';
+import 'package:ift/shared/widgets/app_form_field.dart';
+import 'package:ift/shared/widgets/app_page_scaffold.dart';
+import 'package:ift/shared/widgets/app_section_title.dart';
+import 'package:ift/shared/widgets/app_surface_card.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class ManageAdminLocationsScreen extends StatefulWidget {
   const ManageAdminLocationsScreen({super.key});
