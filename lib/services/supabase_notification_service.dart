c82b1e3bd45b6c013ee0e082f45fe09b19a4fe9d
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 08:14:38 +0100

    Update supabase_notification_service.dart

--- lib/services/supabase_notification_service.dart
+++ lib/services/supabase_notification_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:mix/core/constants/app_constants.dart';
+import 'package:ift/core/constants/app_constants.dart';
 
 class SupabaseNotificationService {
   final http.Client _client;
