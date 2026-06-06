18b7034e39ee51f92c784a84d0cca085fe2bef84
MohammedAuwal <65893816+MohammedAuwal@users.noreply.github.com>
2026-06-06 13:10:10 +0100

    Update paystack_verification_screen.dart

--- lib/features/cart/presentation/screens/paystack_verification_screen.dart
+++ lib/features/cart/presentation/screens/paystack_verification_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mix/models/payment_session_model.dart';
-import 'package:mix/services/firebase_service.dart';
-import 'package:mix/services/payment_service.dart';
-import 'package:mix/shared/widgets/app_page_scaffold.dart';
-import 'package:mix/shared/widgets/app_surface_card.dart';
-import 'package:mix/core/theme/build_context_theme_x.dart';
+import 'package:ift/models/payment_session_model.dart';
+import 'package:ift/services/firebase_service.dart';
+import 'package:ift/services/payment_service.dart';
+import 'package:ift/shared/widgets/app_page_scaffold.dart';
+import 'package:ift/shared/widgets/app_surface_card.dart';
+import 'package:ift/core/theme/build_context_theme_x.dart';
 
 class PaystackVerificationScreen extends StatefulWidget {
   final PaymentSessionModel session;
